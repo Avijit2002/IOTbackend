39459fba21eaf3a7a75751a1d848c6a429300e69
Avijit <avijitram2013@gmail.com>
2025-04-16 20:22:37 +0530

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import bodyParser from "body-parser";
+
+const app = express()
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/',(req,res,next)=>{
+    res.send("hii");
+})
+
+app.post('/data',(req,res,_)=>{
+    console.log(req.body);
+    res.send("data reached")
+})
+
+app.listen(3000,()=>{
+    console.log("server started")
+})
