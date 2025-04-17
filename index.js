9d792e7f4d613a34252f720bf2ed23990c23c6dd
Avijit <avijitram2013@gmail.com>
2025-04-17 20:54:17 +0530

    update

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 import express from "express"
 import bodyParser from "body-parser";
 
+const moisture = 0;
+const vibration = 0;
+const rain = 0;
+
+
 const app = express()
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/',(req,res,next)=>{
-    res.send("hii");
+    res.json({
+        moisture,vibration,rain
+    });
 })
 
 app.post('/data',(req,res,_)=>{
-    console.log("hi")
+    //console.log("hi")
     console.log(req.body);
     res.send("data reached")
+
+    moisture = req.body.moisture;
+    vibration = req.body.vibration;
+    rain = req.body.rain;
 })
 
 app.listen(3000,()=>{
